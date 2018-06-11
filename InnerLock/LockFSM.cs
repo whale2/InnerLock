5e114d2a2673c2c18363a486925bd33ee616f819
whale2 <whale2.box@gmail.com>
2018-06-11 18:39:20 +0200

    FSM dummy

---
+++ InnerLock/LockFSM.cs
@@ -0,0 +1,42 @@
+﻿namespace InnerLock
+{
+    public class LockFSM
+    {
+        // Lock Finite State Machine   
+
+        public enum State
+        {
+            Idle,
+            Ready,
+            Locking,
+            Locked,
+            Unlocking,
+            Broken
+        }
+
+        public enum Transition
+        {
+            Engage,
+            OnContact,
+            OnSlip,
+            SuccessfulLock,
+            DisengageFromReady,
+            DisengageFromLocking,
+            DisengageFromLocked,
+            SuccessfulUnlock,
+            Breaking
+        }
+
+        public State state;
+
+        public LockFSM()
+        {
+            state = State.Idle;
+        }
+
+        public LockFSM(State initialState)
+        {
+            state = initialState;
+        }
+    }
+}
