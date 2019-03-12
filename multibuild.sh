e957796ec747534299bfae7e16eabecb3c6b135f
Nikita Makeev <whale2.box@gmail.com>
2019-03-13 00:45:50 +0100

    Prepare release 0.0.4 Add .version file autopatching

--- multibuild.sh
+++ multibuild.sh
@@ -22,16 +22,32 @@ for t in ${TARGETS}; do
     
     cp -r . ${TMPDIR}
     cd ${TMPDIR}
+    
+    # Patch csproj
     export ASSEMBLY_PATH=$(echo ${KSP_DIR}|sed -e 's%\/%\\%g')
     cat ${PROJECT}/${PROJECT}.csproj|\
       perl -ne '$V=$ENV{"ASSEMBLY_PATH"}; s/<HintPath>(.*\\ksp-versions\\)(ksp-[^\\]+)(\\.*)/<HintPath>$V$3/; print $_' > tmp.csproj
     mv tmp.csproj ${PROJECT}/${PROJECT}.csproj
 
+    # Build .dll
     KSP_BUILD_VERSION=$(echo $t|sed -e s'/\.//g')
     export SolutionDir=$(pwd)
     ${MSBUILD} /p:DefineConstants="KSP_${KSP_BUILD_VERSION}"
     PACKAGE="${PROJECT}-${PROJECT_VERSION}-ksp-$t.zip"
-    zip -r ${PACKAGE} GameData
+    
+    # Patch .version
+    KSP_VERSION_MAJOR=$(echo $t|cut -d. -f1)
+    KSP_VERSION_MINOR=$(echo $t|cut -d. -f2)
+    KSP_VERSION_PATCH=$(echo $t|cut -d. -f3)
+
+    cat GameData/${PROJECT}/$PROJECT.version|\
+      jq ".KSP_VERSION.MAJOR=${KSP_VERSION_MAJOR}|.KSP_VERSION.MINOR=${KSP_VERSION_MINOR}|.KSP_VERSION.PATCH=${KSP_VERSION_PATCH}\
+      |.KSP_VERSION_MIN.MAJOR=${KSP_VERSION_MAJOR}|.KSP_VERSION_MIN.MINOR=${KSP_VERSION_MINOR}|.KSP_VERSION_MIN.PATCH=0\
+      |.KSP_VERSION_MAX.MAJOR=${KSP_VERSION_MAJOR}|.KSP_VERSION_MAX.MINOR=${KSP_VERSION_MINOR}|.KSP_VERSION_MAX.PATCH=99" > tmp.version
+    mv tmp.version GameData/${PROJECT}/$PROJECT.version
+
+    # Build package
+    zip -r ${PACKAGE} GameData -x \*~\*
     mv ${PACKAGE} ${SOURCE_DIR}
     cd ${SOURCE_DIR}
     rm -rf ${TMPDIR}
